185ad7f19d9097986fdd8f872bbea240aaef5d42
kennethfan <kennethfan137@gmail.com>
2025-10-11 16:23:55 +0800

    异步

--- crates/single-thread-web-server/src/main.rs
+++ crates/single-thread-web-server/src/main.rs
@@ -1,32 +1,33 @@
-use std::env;
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
+use async_std::io::WriteExt;
+use async_std::io::ReadExt;
+use async_std::net::{TcpListener, TcpStream};
 use async_std::task;
+use futures::StreamExt;
+use std::env;
+use std::io::Read;
+use std::time::Duration;
 
 static mut WEB_ROOT: String = String::new();
 
 #[async_std::main]
 async fn main() {
-    println!("Hello, world!");
-
     unsafe {
         WEB_ROOT = env::var("WEB_ROOT".to_string()).unwrap_or("".to_string());
     }
 
-    let listener = TcpListener::bind("127.0.0.1:6000").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream).await;
-    }
+    let listener = TcpListener::bind("127.0.0.1:6000").await.unwrap();
+    println!("listening!");
+    listener.incoming()
+        .for_each_concurrent(None, |stream| async move {
+            let stream = stream.unwrap();
+            handle_connection(stream).await;
+        }).await;
 }
 
 
 async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -47,6 +48,6 @@ async fn handle_connection(mut stream: TcpStream) {
 
     let response = format!("{}{}", status_line, contents);
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
