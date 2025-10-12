bcaedb83e968e73d3b052b61662a272bf81172b8
kennethfan <kennethfan137@gmail.com>
2025-10-12 15:48:52 +0800

    带回结果

--- crates/my-redis/src/bin/client.rs
+++ crates/my-redis/src/bin/client.rs
@@ -1,15 +1,19 @@
 use bytes::Bytes;
 use mini_redis::client;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[derive(Debug)]
 enum Command {
     Get {
         key: String,
+        resp: Responder<Option<Bytes>>,
     },
     Set {
         key: String,
         val: Bytes,
+        resp: Responder<()>,
     },
 }
 #[tokio::main]
@@ -23,11 +27,13 @@ async fn main() {
         while let Some(cmd) = rx.recv().await {
             use Command::*;
             match cmd {
-                Get { key } => {
-                    client.get(&key).await;
+                Get { key, resp } => {
+                    let result = client.get(&key).await;
+                    let _ = resp.send(result);
                 }
-                Set { key, val } => {
-                    client.set(&key, val).await;
+                Set { key, val, resp } => {
+                    let result = client.set(&key, val).await;
+                    let _ = resp.send(result);
                 }
             }
         }
@@ -35,20 +41,33 @@ async fn main() {
 
     // 生成两个任务，一个用于获取 key，一个用于设置 key
     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
             key: "hello".to_string(),
+            resp: resp_tx,
         };
 
+        // 发送 GET 请求
         tx.send(cmd).await.unwrap();
+
+        // 等待回复
+        let result = resp_rx.await.unwrap();
+        println!("GOT = {:?}", result);
     });
 
     let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            resp: resp_tx,
         };
 
+        // 发送 SET 请求
         tx2.send(cmd).await.unwrap();
+        // 等待回复
+        let result = resp_rx.await.unwrap();
+        println!("GOT = {:?}", result);
     });
 
     t1.await.unwrap();
