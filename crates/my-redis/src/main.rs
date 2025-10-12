a37f0489700896bc4c6694f47143328a545591f0
kennethfan <kennethfan137@gmail.com>
2025-10-12 15:18:15 +0800

    共享状态

--- crates/my-redis/src/main.rs
+++ crates/my-redis/src/main.rs
@@ -1,11 +1,18 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 const REDIS_HOST: &str = "REDIS_HOST";
 const REDIS_PORT: &str = "REDIS_PORT";
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
+
+
     let mut addr = std::env::var(REDIS_HOST.to_string()).unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = std::env::var(REDIS_PORT.to_string()).unwrap_or_else(|_| "6379".to_string());
     // 建立与mini-redis服务器的连接
@@ -13,24 +20,23 @@ async fn main() {
     addr.push_str(&port);
 
     let listener = TcpListener::bind(&addr).await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         // process(socket).await;
+        let db = db.clone();
         // 为每一条连接都生成一个新的任务，
         // `socket` 的所有权将被移动到新的任务中，并在那里进行处理
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db :Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // 使用 hashmap 来存储 redis 的数据
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
@@ -39,10 +45,12 @@ async fn process(socket: TcpStream) {
 
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
