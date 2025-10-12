37b5f4c8162706e2f52bef18f342497c008e2c5d
kennethfan <kennethfan137@gmail.com>
2025-10-12 15:02:05 +0800

    初始化server

--- crates/my-redis/src/main.rs
+++ crates/my-redis/src/main.rs
@@ -1,24 +1,34 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 const REDIS_HOST: &str = "REDIS_HOST";
 const REDIS_PORT: &str = "REDIS_PORT";
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut addr = std::env::var(REDIS_HOST.to_string()).unwrap_or_else(|_| "127.0.0.1".to_string());
+async fn main() {
+    let mut addr = std::env::var(REDIS_HOST.to_string()).unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = std::env::var(REDIS_PORT.to_string()).unwrap_or_else(|_| "6379".to_string());
     // 建立与mini-redis服务器的连接
     addr.push_str(":");
     addr.push_str(&port);
-    let mut client = client::connect(addr).await?;
 
-    // 设置 key: "hello" 和 值: "world"
-    client.set("hello", "world".into()).await?;
+    let listener = TcpListener::bind(&addr).await.unwrap();
 
-    // 获取"key=hello"的值
-    let result = client.get("hello").await?;
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
 
-    println!("从服务器端获取到结果={:?}", result);
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
 
-    Ok(())
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Got {:?}", frame);
+
+
+        // 回复一个错误
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
